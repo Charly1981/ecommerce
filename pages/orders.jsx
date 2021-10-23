d1444e1b0416c7222fffb8bcc27e6b0fe7f707f5
Charly <charly81.zirpoli@gmail.com>
2021-10-23 12:43:10 -0300

    orders page

---
+++ pages/orders.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BasicLayout from "../layouts/BasicLayout";
+
+export default function orders() {
+  return (
+    <BasicLayout className="orders">
+      <div className="orders__block">
+        <div className="title">Mis Pedidos</div>
+        <div className="data">
+          <p>Lista de pedidos</p>
+        </div>
+      </div>
+    </BasicLayout>
+  );
+}
